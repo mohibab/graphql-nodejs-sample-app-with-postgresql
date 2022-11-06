528be9d8854ae48f7847a9c115456ec58e14436a
Mohibur Rahman <mohib@bs-23.net>
2022-11-06 16:28:16 +0600

    added graphql filter by id

---
+++ data.js
@@ -0,0 +1,42 @@
+module.exports = {
+     userList: [
+        {
+            id: "1",
+            name: "Test",
+            address:{
+                city: 'Dhaka',
+                street:"19/A1",
+                post: '15210'
+            }
+        },
+        {
+            id: "2",
+            name: "Test 2",
+            address:{
+                city: 'Dhaka 2',
+                street:"20/A1",
+                post: '56895'
+            }
+        }
+    ],
+    post: [
+        {
+            id: '1',
+            title: 'Post 1',
+            content: 'Hello World',
+            comment: 'nice Post'
+        },
+        {
+            id: '2',
+            title: 'Post 2',
+            content: 'Hello GraphGL',
+            comment: 'Nice Post'
+        }
+    ],
+    followers: [
+        {
+            id: '1',
+            name: 'Admin',
+        }
+    ]
+}
--- resolver.js
+++ resolver.js
@@ -1,37 +1,17 @@
+const {userList, post, followers} = require('./data')
+
 module.exports = {
-    getUser: function (){
-        return({
-            id: "1",
-            name: "Test",
-            address:{
-                city: 'Dhaka',
-                street:"19/A1",
-                post: '15210'
-            }
-        })
+    getUser: function (params){
+        console.log(params);
+        let result = userList.filter(item => item.id == params.id)
+        return result[0];
     },
-    getPost: function (){
-        return([
-            {
-                id: '1',
-                title: 'Post 1',
-                content: 'Hello World',
-                comment: 'nice Post'
-            },
-            {
-                id: '2',
-                title: 'Post 2',
-                content: 'Hello GraphGL',
-                comment: 'Nice Post'
-            }
-        ])
+    getPost: function (params){
+        let result = post.filter(item => item.id == params.id)
+        return result[0];   
     },
-    getFollowers: function (){
-        return([
-            {
-                id: '1',
-                name: 'Admin',
-            }
-        ])
+    getFollowers: function (params){
+        let result = followers.filter(item => item.id == params.id)
+        return result[0];
     }
 }
